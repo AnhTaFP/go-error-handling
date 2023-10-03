33abcae3703a7a515f8eadd1c3c9dbe32ba5093b
t.viet <ta.viet@foodpanda.com>
2023-10-03 10:39:42 +0800

    Add more tests

--- app/domain/errors/errors_test.go
+++ app/domain/errors/errors_test.go
@@ -2,13 +2,14 @@ package errors
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestErrors(t *testing.T) {
-	t.Run("wrappable", func(t *testing.T) {
+	t.Run("errors.Is", func(t *testing.T) {
 		e1 := Wrap(nil, "foobar", map[string]interface{}{
 			"scope": "scopeA",
 		})
@@ -39,4 +40,30 @@ func TestErrors(t *testing.T) {
 		assert.False(t, errors.Is(emptyErr, x))
 		assert.False(t, errors.Is(x, emptyErr))
 	})
+
+	t.Run("errors.As", func(t *testing.T) {
+		e1 := Wrap(nil, "foobar", nil)
+		e2 := Wrap(e1, "foobar 2", nil)
+		e3 := fmt.Errorf("foobar 3: %w", e2)
+
+		var e4 *Error
+		assert.True(t, errors.As(e3, &e4))
+		assert.Equal(t, "foobar 2: foobar", e4.Error())
+
+		ce := customErr{msg: "custom foobar"}
+		e5 := Wrap(ce, "foobar 5", nil)
+		e6 := Wrap(e5, "foobar 56", nil)
+
+		var ce2 customErr
+		assert.True(t, errors.As(e6, &ce2))
+		assert.Equal(t, "custom foobar", ce2.Error())
+	})
+}
+
+type customErr struct {
+	msg string
+}
+
+func (ce customErr) Error() string {
+	return ce.msg
 }
